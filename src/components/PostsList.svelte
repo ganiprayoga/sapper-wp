ec2549d936a4b5953cb24e60e69b4c0c451bce34
Gani Prayoga <gani.prayoga@gmail.com>
2020-04-28 13:17:59 +0700

    Second Commit

---
+++ src/components/PostsList.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { onMount } from 'svelte'
+
+    let posts = []
+
+    const apiUrl = process.env.SAPPER_APP_API_URL
+
+    onMount(async () => {
+        const res = await fetch(`${apiUrl}/wp/v2/posts`)
+        const json = await res.json()
+        posts = json
+
+        // console.log(posts)
+    })
+</script>
+
+<h1>Isi Artikel terbaru Gani Prayoga's Weblog</h1>
+
+<ul>
+    {#each posts as post}
+        <li>
+            <a rel="prefetch" href="articles/{post.slug}">{post.title.rendered}</a>
+        </li>
+    
+    {/each}
+</ul>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Sapper Wordpress</title>
+</svelte:head>
+
+<script>
+	import PostsList from '../components/PostsList.svelte'
+</script>
+
+<PostsList />
